718a9b1421acc24a9461af44a40fb2a52119e6be
Dmyto Haidash <dmyto.haidash@gmail.com>
2019-11-25 13:19:18 +0200

    mail file

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'mr.nihonto@mail.ru'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@spheredigitech.com'),
         'name' => env('MAIL_FROM_NAME', 'Soshuden'),
     ],
 
