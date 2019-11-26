6cc52c73eb4bb24a90aa2654349d3efe9029b5f8
Dmyto Haidash <dmyto.haidash@gmail.com>
2019-11-26 12:58:31 +0200

    #

--- resources/views/client/home/partials/sections.blade.php
+++ resources/views/client/home/partials/sections.blade.php
@@ -25,7 +25,7 @@
                                         </div>
 
                                         @if ($child->hasTranslation('description'))
-                                            <div class="teaser__description mt-3 px-6 lg:px-10">
+                                            <div class="teaser__description mt-3 px-6 lg:px-10 text-3xl">
                                                 {{ Str::limit($child->description, 150) }}
                                             </div>
                                         @endif
